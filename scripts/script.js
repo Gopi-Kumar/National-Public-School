fdefa85e22e69c3ca381c10c470a0e555c59173e
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-08-11 01:55:50 +0530

    image slide added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,29 @@
+//Image slide 
+let imgsrc = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,19,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35];
+let img = document.querySelector(".img_slide_show img");
+
+img.src = './images/1.jpg';
+let numberOfImage = imgsrc.length;
+console.log(numberOfImage)
+let i=0;
+function prevImage(){
+   if(i <= 0){
+       i = imgsrc.length;
+   }
+    i -= 1;
+   img.src = `./images/${imgsrc[i]}.jpg`
+}
+function nextImage(){
+    if(i == imgsrc.length - 1){
+        i = -1;
+    }
+     i += 1;
+     console.log(i)
+     img.src = `./images/${imgsrc[i]}.jpg`
+
+}
+setInterval(nextImage, 3500)
+//Message box
 let endpoint  = "http://localhost:3002"
 function sendMessage(){
     let name = document.getElementById("name").value,
